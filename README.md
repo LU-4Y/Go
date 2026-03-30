c1dc25c6790ab19f39e0659af60c8967e9344f62
LU-4Y <121696551+LU-4Y@users.noreply.github.com>
2026-03-30 00:03:12 -0400

    Added some notes to the readme file to track how I want to tackle liberties, scoring points, etc.

--- README.md
+++ README.md
@@ -19,3 +19,18 @@ Version 2.0 Project requirements:
 Version 3.0 Project requirements:  
     >   Implemented GUI  
     >   P2P over LAN  
+
+==========================================  
+==========================================  
+
+29MAR2026
+Notes on connecting pieces for capture and point logic.
+When pieces are on a board, they intrinsically "connect" to adjacent pieces.  I will need to write an algorithm to determine the contiguous bodies on the board.  The way I am thinking of tackling that right now is using a "growing tree" model.  
+
+When a piece is placed, it performs a check in all four direction of itself.  Where it finds another piece of the same color, it grows a "branch" to connect with that, joining to that contiguous body.  Where it finds empty space, a "+", it has a liberty (can also be considered a branch, but of a different type).  
+
+As I type this out, I think I should have a "contiguous body" class which has specified coordinates on the board, and each time a piece is placed, it checks if it touches a contiguous body, and the contiguous body object is updated to include it.  This will help manage liberties; when all the liberties of a contiguous body are filled, the coordinates of the contiguous body is replaced with "+", and the object is then deleted.  If the piece is solitary, it should not need to instantiate a contiguous object class.  When two contiguous bodies connect, they form a new one.  
+
+It seems like we can track how a contiguous body changes by tracking what piece is placed in a liberty coordinate.
+
+To calculate points, it may be necessary to calculate inverse liberties of "+" spaces as well.
--- classes.py
+++ classes.py
@@ -17,28 +17,27 @@ class Board:
         
         self.size = size
         self.layout = [["+" for x in range(size)] for x in range(size)]
-        self.color = Pieces()
+        self.color = Pieces()  #Do we need this line?  Or should I declare the color class in the main file?
         self.key = {}
 
         #generate grid key for board
-        rows_low = string.ascii_lowercase[:size]
-        rows_upp = string.ascii_uppercase[:size]
+        rows = string.ascii_uppercase[:size] #NOTE: you will need to convert coordinates to upper case using .upper() to fit the key.
         for n in range(size):  #I know this is t = O(n^2). I don't know if I want to fix it because the max grid size is 25.
             for m in range(size):
-                self.key_low[rows_low[n] + str(m)] = [n,m]
-                self.key_upp[rows_upp[n] + str(m)] = [n,m]
+                self.key[rows[n] + str(m)] = [n,m] #Creates a dictionary entry that connects to every coordinate location in layout.  "A1" maps to [1,1], "A2" maps to [1,2], etc.
         #generate grid key for board
 
-    def place_piece(self, place):  #place is a list.  The first item is the turn, the second item is the grid coordinate.
+    def place_piece(self, place) -> int:  #place is a list.  The first item is the turn, the second item is the grid coordinate.
         #unpack place variable
-        color = place[0]
-        coord = place[1]
+        player = place[0]
+        coord = place[1].upper() ##upper() ensures compatability with Board.key dict
         #unpack place variable
 
-        #convert coord to layout parameters
+        k = 0 #initialization within mathod. This variable will determine whether the turn moves forward or not.  k=0 is returned from this method if something is wrong, k=1 is returned from this method if a piece is successfully played on the board.
 
-        #convert coord to layout parameters
+        coord = self.key[coord] #convert coord to layout parameters
 
-        ##check if valid move
-        
+        #check if valid move
+        if (self.layout[coord[0]][coord[1]] != "+"):
+            return k
         #check if valid move
